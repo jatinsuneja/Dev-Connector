62142f82eae191ebc37a3ace42b5d641e7b90354
Jatin Suneja <jatinsuneja16@gmail.com>
2020-06-05 23:57:00 +0530

    Final Commit

---
+++ build/precache-manifest.dc6ec306ffd3fa7c25ded122c20f83d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d662eb85e209c245e7182243e3f5c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef69938ef32db689d20a",
+    "url": "/static/css/main.60a30dc2.chunk.css"
+  },
+  {
+    "revision": "2d8e727f0d385462941a",
+    "url": "/static/js/2.ab35e7e3.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.ab35e7e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef69938ef32db689d20a",
+    "url": "/static/js/main.2d888274.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
